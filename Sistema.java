178016131443a29d92f8a1928c27d7f626e31dcd
Nagel <130867271+Nagel-P@users.noreply.github.com>
2024-05-04 13:20:15 -0300

    Update Sistema.java

--- Sistema.java
+++ Sistema.java
@@ -7,7 +7,23 @@ interface Trabalhavel {
 }
 
 abstract class Funcionario implements Trabalhavel {
-    
+    private String nome;
+    private String matricula;
+
+    public Funcionario(String nome, String matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public abstract double calcSalario();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
 }
 
 class Gerente extends Funcionario {
