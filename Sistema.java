e0c55331b2dd9309751b24ba5466dd01e81740b6
Nagel <130867271+Nagel-P@users.noreply.github.com>
2024-05-04 13:18:24 -0300

    Update Sistema.java

--- Sistema.java
+++ Sistema.java
@@ -1,1 +1,23 @@
+import java.util.ArrayList;
 
+interface Trabalhavel {
+    void trabalhar();
+
+    void relatar();
+}
+
+abstract class Funcionario implements Trabalhavel {
+    
+}
+
+class Gerente extends Funcionario {
+    
+}
+
+class Desenvolvedor extends Funcionario {
+    
+}
+
+class Estagiario extends Funcionario {
+    
+}
