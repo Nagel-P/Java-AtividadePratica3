06cca86a2b9a881c31fa7b8ef17a925f68887a74
Nagel <130867271+Nagel-P@users.noreply.github.com>
2024-05-04 12:52:58 -0300

    Update Console.java

--- Console.java
+++ Console.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class Console {
+    private static Scanner leitor = new Scanner(System.in);
+
+    public static int lerInt() {
+        int valor = leitor.nextInt();
+        leitor.nextLine();
+        return valor;
+    }
+
+    public static String lerString() {
+        String txt = leitor.nextLine();
+        return txt;
+    }
+
+    public static float lerFloat() {
+        float valor = leitor.nextFloat();
+        leitor.nextLine();
+        return valor;
+    }
+
+    public static char lerChar() {
+        char valor = leitor.next().charAt(0);
+        leitor.nextLine();
+        return valor;
+    }
+
+}
